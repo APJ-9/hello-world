aa3a67e0f12a2751155cfaa632bf948a09962103
APJ-9 <akhil.jose@vonnue.com>
2022-10-22 16:26:45 +0530

    added a data.js file which is similar to a json file

--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
+import PortfolioList from '../portfolioList/PortfolioList';
 import './Portfolio.css'
 
 function Portfolio() {
+  const [selected, setSelected] = useState('featured')
+    const list = [
+        {
+          id: "featured",
+          title: "Featured",
+        },
+        {
+          id: "web",
+          title: "Web App",
+        },
+        {
+          id: "mobile",
+          title: "Mobile App",
+        },
+        {
+          id: "design",
+          title: "Design",
+        },
+        {
+          id: "content",
+          title: "Content",
+        },
+      ];
   return (
     <div className='portfolio' id='portfolio'>
+        <h1>Portfolio</h1>
+        <ul>
+            {
+                list.map((item)=>(
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+                ))
+            }
+        </ul>
+        <div className="container">
+            <div className="item">
 
+            </div>
+        </div>
     </div>
   )
 }
