6f448e88b665727547df83ac59fe98824f6b729c
APJ-9 <akhil.jose@vonnue.com>
2022-10-30 13:03:48 +0530

    tutorial 13

--- Gatsby/project/src/pages/projects/index.js
+++ Gatsby/project/src/pages/projects/index.js
@@ -1,17 +1,46 @@
 
+import { graphql, Link } from 'gatsby';
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
 
-const Projects = () => {
+export default function Projects({ data }) {
+    // console.log(data)
+    const projects = data.allMarkdownRemark.nodes
+    console.log(projects)
     return (
         <Layout>
             <div className={styles.portfolio}>
                 <h2>Portfolio</h2>
                 <h3>Projects & Websites I've Created</h3>
             </div>
+            <div className={styles.projects}>
+                {
+                    projects.map(project => (
+                        <Link to={`/projects/${project.frontmatter.slug}`} key={project.id} >
+                            <div>
+                                <h3>{project.frontmatter.title}</h3>
+                                <p>{project.frontmatter.stack}</p>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
         </Layout>
     );
 }
 
-export default Projects
+export const query = graphql`
+query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        id
+        frontmatter {
+          slug
+          stack
+          title
+        }
+      }
+    }
+  }  
+`
