95aa9e884242a20a0d94a63d6a5139cc87173175
APJ-9 <akhil.jose@vonnue.com>
2022-11-04 23:03:46 +0530

    added an array

--- Task/JS/multiplication.js
+++ Task/JS/multiplication.js
@@ -2,7 +2,7 @@ console.log("*****     SINGLE LINE     *****")
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         let product = (i * j).toString()
-        process.stdout.write(j.toString() + " * " + i.toString() + " = " + product + "  ")
+        // process.stdout.write(j.toString() + " * " + i.toString() + " = " + product + "  ")
     }
 }
 
@@ -10,8 +10,10 @@ console.log("\n\n*****     MULTIPLE LINE BREAKS AFTER 9     *****")
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         let product = (i * j).toString()
-        process.stdout.write(j.toString() + " * " + i.toString() + " = " + product.padStart(2, 0) + "  ")
+        // process.stdout.write(j.toString() + " * " + i.toString() + " = " + product.padStart(2, 0) + "  ")
     }
-    process.stdout.write('\n')
+    // process.stdout.write('\n')
 }
 
+const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(numberArray)
